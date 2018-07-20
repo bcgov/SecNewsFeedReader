ac842b9df477bce202b4e5c8561685f6e8fdf959
Rai <PRRAI@BCGOV>
2018-07-20 16:06:16 -0700

    All feeds working

--- FeedReader/views.py
+++ FeedReader/views.py
@@ -67,9 +67,9 @@ def home(request):
 
 
     XML_LINKS= ['https://nakedsecurity.sophos.com/feed/', 'http://feeds.feedburner.com/govinfosecurity/com', 'http://feeds.feedburner.com/SecurityIntelligence']
-    #for object in objects:
-       #for title,xmls in object.sub_headings:
-            #XML_LINKS.append(xmls)
+    for object in objects:
+        for title,xmls in object.sub_headings:
+             XML_LINKS.append(xmls)
 
     Feed=[]
 
@@ -79,7 +79,10 @@ def home(request):
             f = Feeds()
             f.title = post.title
             f.link = post.link
-            f.description = post.description
+            try:
+                f.description=post.description
+            except:
+                f.description = ''
             Feed.append(f)
 
 
